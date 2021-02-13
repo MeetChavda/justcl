45f5696bcf6047d56d72f433049f1eb9e195abfb
meet <chavdameet493@gmail.com>
2021-02-13 19:35:18 +0530

    updated project work all page completly

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+    Route::get('/', function () {
+        return view('index');
+    });
+    Route::get('about', function () {
+        return view('about');
+    });
+    Route::get('agents-grid', function () {
+        return view('agents-grid');
+    });
+    Route::get('agent-single', function () {
+        return view('agent-single');
+    });
+    Route::get('blog-grid', function () {
+        return view('blog-grid');
+    });
+    Route::get('blog-single', function () {
+        return view('blog-single');
+    });
+    Route::get('contact', function () {
+        return view('contact');
+    });
+    Route::get('property-grid', function () {
+        return view('property-grid');
+    });
+    Route::get('property-single', function () {
+        return view('property-single');
+    });
